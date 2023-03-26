8ce774c93d908dcbaae27be03137c71a20eb8a07
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2023-03-26 16:02:02 +0900

    a

---
+++ src/pages/krunker-css/1.css.js
@@ -0,0 +1,7 @@
+export async function get() {
+    const response = await fetch('https://raw.githubusercontent.com/shaaaaaQ/chrome-ext/main/src/krunker/style.css');
+    const css = await response.text()
+    return {
+        body: css
+    }
+}
