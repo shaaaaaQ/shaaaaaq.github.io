4407de3078dc9408cea9538f422b8c96b5b12849
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2023-03-29 23:16:21 +0900

    a

--- src/apps/genshin-score-uid/characters.js
+++ src/apps/genshin-score-uid/characters.js
@@ -1,4 +1,4 @@
-export default await (async () => {
-    const res = await fetch('https://raw.githubusercontent.com/EnkaNetwork/API-docs/master/store/characters.json');
-    return res.json();
-})();
+const res = await fetch('https://raw.githubusercontent.com/EnkaNetwork/API-docs/master/store/characters.json');
+const characters = await res.json()
+
+export default characters
--- src/apps/genshin-score-uid/loc.js
+++ src/apps/genshin-score-uid/loc.js
@@ -1,4 +1,4 @@
-export default await (async () => {
-    const res = await fetch('https://raw.githubusercontent.com/EnkaNetwork/API-docs/master/store/loc.json');
-    return res.json();
-})();
+const res = await fetch('https://raw.githubusercontent.com/EnkaNetwork/API-docs/master/store/loc.json');
+const loc = await res.json()
+
+export default loc
