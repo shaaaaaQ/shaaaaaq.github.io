d29000ed4fff190a2eef3fbd0ff7245c7294b944
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2023-03-29 23:07:13 +0900

    a

--- src/apps/genshin-score-uid/characters.js
+++ src/apps/genshin-score-uid/characters.js
@@ -1,5 +1,4 @@
 export default await (async () => {
-    console.log('characters')
     const res = await fetch('https://raw.githubusercontent.com/EnkaNetwork/API-docs/master/store/characters.json');
     return res.json();
 })();
--- src/apps/genshin-score-uid/loc.js
+++ src/apps/genshin-score-uid/loc.js
@@ -1,5 +1,4 @@
 export default await (async () => {
-    console.log('loc')
     const res = await fetch('https://raw.githubusercontent.com/EnkaNetwork/API-docs/master/store/loc.json');
     return res.json();
 })();
