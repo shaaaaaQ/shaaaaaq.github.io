1380d830c41c5d223ae1d7ea6e08c6a2f8af801a
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2024-05-05 23:04:29 +0900

    a

--- src/pages/krunker-css/1.css.js
+++
@@ -1,7 +0,0 @@
-export async function get() {
-    const response = await fetch('https://raw.githubusercontent.com/shaaaaaQ/chrome-ext/main/src/krunker/style.css');
-    const css = await response.text();
-    return {
-        body: css
-    };
-}
---
+++ src/pages/nanka.md
@@ -0,0 +1,8 @@
+---
+layout: ../layouts/Md.astro
+title: なんか
+---
+## 作ったやつ？
+- [osu!maniaのあれを真ん中にするときのColumnStartを求めるやつ](/mania-centering/index.html)
+- [btoa, atob](/btoa/index.html)
+- [聖遺物の画像からスコア計算するやつ](/genshin-score/index.html)
